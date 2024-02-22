f4a5c7492202c4b735a51481fe49fcfd10001e32
xgrn <xgrn119@gmail.com>
2024-02-23 01:13:31 +0300

    Routes added.

--- src/Kernel/RouteAssigner.php
+++ src/Kernel/RouteAssigner.php
@@ -3,6 +3,7 @@
 namespace App\Kernel;
 
 use App\Controller\Article\FetchController;
+use App\Controller\Article\UpdateController;
 use Slim\App;
 
 class RouteAssigner
@@ -10,6 +11,8 @@ class RouteAssigner
 
     public static function addRoutes(App $app): void
     {
-        $app->get('/', [FetchController::class, 'fetch'])->setName('app_articles_fetch');
+        $app->get('/', [FetchController::class, 'fetchAll'])->setName('app_articles_fetch_all');
+        $app->get('/article/{filename}', [FetchController::class, 'fetchArticle'])->setName('app_article_fetch');
+        $app->put('/article/{filename}', [UpdateController::class, 'update'])->setName('app_article_update');
     }
 }
